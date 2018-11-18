9ceb0abd53c75fffaea3916bc5d8ed326af2230d
陈洋 <chenyang@chenyangdeMacBook-Pro.local>
2018-11-18 18:58:22 +0800

    mybatis逆向工程

---
+++ myproject5/src/main/java/com/zte/web/myproject5/Myproject5Application.java
@@ -0,0 +1,12 @@
+package com.zte.web.myproject5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myproject5Application.class, args);
+	}
+}
